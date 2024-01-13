f27b2504477c29494e665c05b41546cba3c67f9b
Süleyman Mutlu <suleyman.mutlu96@gmail.com>
2024-01-13 12:12:35 +0300

    package fix

--- test/book.test.js
+++ test/book.test.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../app');
-let bookId
+let bookId;
 describe('GET /book', () => {
   it('should return all books', () => {
     request(app)
@@ -30,37 +30,35 @@ describe('POST /book', () => {
         language: 'TR',
         numberOfPages: 133,
         publisher: 'Süleyman',
-      }).expect(201)
+      })
+      .expect(201);
   });
 });
 
-describe('PUT /book',()=>{
-    it('should update a product',()=>{
-        request(app)
-        .put('/book')
-        .send({
-            bookId:123123123123123123123123,
-            title: 'Deneme',
-            author: {
-              name: 'Deneme Yazar',
-              country: 'Finlandiya',
-              birthDate: '21.11.1996',
-            },
-            price: 1800,
-            isbn: 1313131311331,
-            language: 'TR',
-            numberOfPages: 133,
-            publisher: 'Süleyman',
-        })
-        .expect(200)
-    })
-})
+describe('PUT /book', () => {
+  it('should update a product', () => {
+    request(app)
+      .put('/book')
+      .send({
+        bookId: 123123123123123123123123,
+        title: 'Deneme',
+        author: {
+          name: 'Deneme Yazar',
+          country: 'Finlandiya',
+          birthDate: '21.11.1996',
+        },
+        price: 1800,
+        isbn: 1313131311331,
+        language: 'TR',
+        numberOfPages: 133,
+        publisher: 'Süleyman',
+      })
+      .expect(200);
+  });
+});
 
-describe('DELETE /book/:bookId',()=>{
-    it('Should delete a product',()=>{
-        request(app)
-        .delete(`/book/:${bookId}`)
-        .expect(200)
-    })
-})
-
+describe('DELETE /book/:bookId', () => {
+  it('Should delete a product', () => {
+    request(app).delete(`/book/:${bookId}`).expect(200);
+  });
+});
